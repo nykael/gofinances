d66890e28fc0ae96fbde4a82eeaed7b10d63a64b
nykael <nykaelccardial@gmail.com>
2022-04-05 15:17:05 -0300

    header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,36 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
 
-import { Container,Title } from "./styles";
+import { 
+  Container,
+   Header,
+   UserWrapper,
+   UserInfo,
+   Photo,
+   User,
+   UserGreeting,
+   UserName,
+   Incon
+} from "./styles";
 
 export function Dashboard() {
   return(
-     <Container>
-       <Title>Dashboard</Title>
+  <Container>
+    <Header>
+
+      <UserWrapper>
+        <UserInfo>
+         <Photo 
+           source={{uri:
+            'https://avatars.githubusercontent.com/u/68310102?v=4' }}/>
+           <User>
+             <UserGreeting>Olá,</UserGreeting>
+             <UserName>Nykael</UserName>
+           </User>
+         </UserInfo>
+         <Incon name='power' />
+       </UserWrapper>
+      
+         </Header>
      </Container>
      
   )
