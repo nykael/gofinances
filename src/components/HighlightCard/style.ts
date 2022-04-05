5c63fcb2f766aeaa8568555e7ab818703c492cb5
nykael <nykaelccardial@gmail.com>
2022-04-05 17:45:51 -0300

    positionCards

--- src/components/HighlightCard/style.ts
+++ src/components/HighlightCard/style.ts
@@ -1,10 +1,15 @@
-import styled from 'styled-components/native'
+import styled, {css} from 'styled-components/native'
 import { RFValue } from 'react-native-responsive-fontsize';
 import {Feather} from '@expo/vector-icons'
 
+type TypeProps = {
+  type: 'up' | 'down'| 'total',
 
-export const Container = styled.View `
-background-color: ${({theme}) => theme.colors.shape} ;
+}
+
+export const Container = styled.View <TypeProps>`
+background-color: ${({theme, type}) =>
+type === 'total' ? theme.colors.secondary : theme.colors.shape};
 width: ${ RFValue(300)}px;
 border-radius:5px;
 
@@ -12,40 +17,53 @@ padding:19px 23px;
 padding-bottom: ${RFValue(42)}px ;
 margin-right: 16px;
 
-height:300px ;
+height:200px ;
 `;
 
 export const Header = styled.View `
   flex-direction:row ;
   justify-content: space-between ;
 `;
-export const Title = styled.Text `
- font-family: ${({theme}) => theme.fonts.medium};
+export const Title = styled.Text <TypeProps>`
+ font-family: ${({theme, type}) => theme.fonts.medium};
  font-size:${RFValue(14)}px ;
 
- color: ${({theme}) => theme.colors.text_dark} ;
+ color: ${({theme, type}) =>
+ type === 'total' ? theme.colors.shape : theme.colors.text_dark};
 
 `;
-export const Icon = styled(Feather) `
+export const Icon = styled(Feather)<TypeProps> `
 font-size: ${RFValue(40)}px ;
 
-color: ${({theme}) => theme.colors.sucess} ;
+ ${({type}) => type ==='up' && css`
+  color:  ${({theme}) => theme.colors.sucess} ;
+ `};
+
+ ${({type}) => type ==='down' && css`
+  color:  ${({theme}) => theme.colors.attention} 
+ `};
+
+ ${({type}) => type ==='total' && css`
+ color:  ${({theme}) => theme.colors.shape} 
+ `};
 `;
 export const Footer = styled.View `
 
 `;
-export const Amount = styled.Text `
-  font-family:${({theme}) => theme.fonts.medium};
+export const Amount = styled.Text <TypeProps> `
+  font-family:${({theme, type}) => theme.fonts.medium};
   font-size: ${RFValue(32)}px;
 
-  color: ${({theme}) => theme.colors.text_dark} ;
+  color: ${({theme, type}) =>
+ type === 'total' ? theme.colors.shape : theme.colors.text_dark};
 
   margin-top: 38px ;
 `;
-export const LastTransaction = styled.Text `
-  font-family:${({theme}) => theme.fonts.regular};
+export const LastTransaction = styled.Text <TypeProps>`
+  font-family:${({theme, type}) => theme.fonts.regular};
   font-size: ${RFValue(12)}px ;
 
-  color: ${({theme})=> theme.colors.text} ;
+ color: ${({theme, type}) =>
+ type === 'total' ? theme.colors.shape : theme.colors.text};
 
 `;
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -34,9 +34,24 @@ export function Dashboard() {
       
      </Header>
      <HighlightCards>
-        <HighlightCard />
-        <HighlightCard />
-        <HighlightCard />
+        <HighlightCard 
+        type="up"
+        title="Entradas" 
+        amount="R$ 17.400,00" 
+        lastTransaction="Última entrada dia 13 de abrial"
+        />
+        <HighlightCard 
+        type="down"
+        title="Saídas" 
+        amount="R$ 1.259,00" 
+        lastTransaction="Última saída dia 03 de abrial"
+        />
+        <HighlightCard 
+        type="total"
+        title="Total" 
+        amount="R$ 16.141,00" 
+        lastTransaction="01 á 16 de abrial"
+        />
      </HighlightCards>
      </Container>
      
