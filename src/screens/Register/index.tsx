c8f1713cd2b37afb23c589c6ba7410e8bb9aa4f2
nykael <nykaelccardial@gmail.com>
2022-04-08 15:48:59 -0300

    Transaction List Responsive

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import { 
   Modal, 
   TouchableWithoutFeedback, 
@@ -7,9 +7,11 @@ import {
   import * as Yup from 'yup'
   import {yupResolver} from '@hookform/resolvers/yup'
   import  AsyncStorage from "@react-native-async-storage/async-storage";
+  import uuid from 'react-native-uuid'
   
   import { useForm } from "react-hook-form";
-
+  import {useNavigation} from '@react-navigation/native'
+  
 import { InputForm } from "../../components/form/InputForm";
 import { Button } from "../../components/form/Button";
 import { TransactionTypeButton  } from "../../components/form/TransactionTypeButton";
@@ -40,11 +42,14 @@ const schema = Yup.object().shape({
   .positive('O valor não pode ser negativo')
 })
 
+interface NavigationProps {
+  navigate: (Screen:string) => void;
+}
+
 export function Register () {
   const [transactionType, setTransactionType] = useState('');
   const [categoryModalOpen,setCategoryModalOpen] = useState(false);
 
-  const dataKey = '@gofinances:transaction'
 
   
   const [category, setCategory] = useState({
@@ -52,9 +57,12 @@ export function Register () {
     name: 'category'
   });
 
+  const navigation = useNavigation<NavigationProps>()
+
   const {
     control,
     handleSubmit,
+    reset,
     formState: {errors}
   } = useForm({
     resolver : yupResolver(schema)
@@ -80,30 +88,40 @@ export function Register () {
     return Alert.alert('Selecione a categotia')
 
 
-    const data = {
+    const newTransaction = {
+      id: String(uuid.v4()),
       name: form.name,
       amount: form.amount,
       transactionType,
-      category: category.key
+      category: category.key,
+      date: new Date()
     }
 
-      try {
-        await AsyncStorage.setItem(dataKey, JSON.stringify(data))
+    try {
+        const dataKey = '@gofinances:transaction'
+        const data = await AsyncStorage.getItem(dataKey);
+        const currentData = data ? JSON.parse(data) : [];
+
+        const dataFormatted =[
+          ...currentData,
+          newTransaction
+        ]
+        await AsyncStorage.setItem(dataKey, JSON.stringify(dataFormatted));
+
+        reset()
+        setTransactionType('')
+        setCategory({
+          key: 'category',
+          name:'Categoria'
+        })
+
+        navigation.navigate('Listagem')
 
       } catch (error) {
         console.log(error)
         Alert.alert('Não foi possível salvar')
       }
   }
-
-  useEffect(() => {
-     async function loadData() {
-      const data = await AsyncStorage.getItem(dataKey);
-      console.log(JSON.parse(data!))
-     }
-     loadData()
-  }, [])
-
 
   return(
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
